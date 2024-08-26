bdcd9f52b2bfa024ec903dcb85f8dd2e14b8dfdf
sataev12 <sataevmagomed.eu@gmail.com>
2024-08-26 10:33:24 +0200

    Création des différents produits via l'administration

--- src/Controller/Admin/TutoCrudController.php
+++ src/Controller/Admin/TutoCrudController.php
@@ -7,6 +7,8 @@ use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\SlugField;
 
 class TutoCrudController extends AbstractCrudController
 {
@@ -15,14 +17,58 @@ class TutoCrudController extends AbstractCrudController
         return Tuto::class;
     }
 
-    /*
+    
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+        $fields =  [
+            ImageField::new('image', 'Image')
+                ->setBasePath('uploads/')
+                ->setUploadDir('public/uploads')
+                ->setUploadedFileNamePattern('[randomhash].[extension]')
+                ->setRequired(true),
         ];
-    }
-    */
+    
+    
+
+        $slug = SlugField::new('slug')->setTargetFieldName('name'); 
+
+        $name = TextField::new('name', 'Titre')
+            ->setFormTypeOptions([
+                'attr' => [
+                    'maxlength' => 255
+                ]
+            ]);
+        
+        $subtitle = TextField::new('subtitle', 'Sous-titre')
+            ->setFormTypeOptions([
+                'attr' => [
+                    'maxlength' => 255
+                ]
+            ]);
+        
+        $video = TextField::new('video', 'Video')
+            ->setFormTypeOptions([
+                'attr' => [
+                    'maxlength' => 255
+                ]
+            ]);
+
+        $link = TextField::new('link', 'Lien')
+            ->setFormTypeOptions([
+                'attr' => [
+                    'maxlength' => 255
+                ]
+            ]);
+            
+        $description = TextEditorField::new('description', 'Description');    
+
+        $fields[] = $name;
+        $fields[] = $subtitle;
+        $fields[] = $slug;
+        $fields[] = $video;
+        $fields[] = $link;
+        $fields[] = $description;
+
+        return $fields;
+    }    
 }
